9cdd3836ef726ac4397475a0e8500ac48fa99436
Guangyu Zhang <zguangyu123@gmail.com>
2015-11-01 22:18:56 +0800

    Query posts from mongodb
    Now you can test it by inserting the following document into your
    database.
    {
      'author': {
    	  'name': 'Guangyu Zhang'
      },
      'html': '<p>Start with this</p>',
      'markdown': 'Start with this',
      'title': 'first test post'
    }

--- epen/route.py
+++ epen/route.py
@@ -1,11 +1,24 @@
 import os
+import pymongo
 from flask import render_template, send_from_directory
 from .app import app, mongo
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    res = mongo.db.posts.find().sort("time", pymongo.DESCENDING)
+    pages = res.count() // 10 + 1
+    next = 2 if pages > 1 else None
+    print(res[0]['author'])
+    return render_template("index.html", posts=res[:10], next=next, pages=pages, page=1)
+
+@app.route("/page/<int:page>/")
+def post_page(page):
+    res = mongo.db.posts.find().sort("time", pymongo.DESCENDING)
+    pages = res.count() // 10 + 1
+    next = page + 1 if pages > page else None
+    previous = page - 1 if page > 1 else None
+    return render_template("index.html", posts=res[(page-1)*10:page*10], next=next, previous=previous, pages=pages)
 
 @app.route("/assets/<path:filename>")
-def custom_static(filename):
+def assets(filename):
     return send_from_directory(os.path.join(os.path.dirname(__file__), "../themes/%s/assets" % (app.config["THEME"],)), filename)
