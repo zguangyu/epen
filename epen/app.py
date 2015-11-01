f2aa5a0d37c76aee7542516715dc75763fe72280
Guangyu Zhang <zguangyu123@gmail.com>
2015-11-01 18:26:33 +0800

    load config from pyfile, to make reload enabled

--- epen/app.py
+++ epen/app.py
@@ -3,7 +3,7 @@ from flask.ext.pymongo import PyMongo
 from jinja2 import FileSystemLoader
 
 app = Flask("epen")
-app.config.from_object("epen.config")
+app.config.from_pyfile("config.py")
 app.jinja_loader = FileSystemLoader("themes/%s" % (app.config["THEME"],))
 
 mongo = PyMongo(app)
