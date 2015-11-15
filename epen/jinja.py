194ac2375ce21259f4d0a79681a62c43c73e78cd
Guangyu Zhang <zguangyu123@gmail.com>
2015-11-15 17:57:13 +0800

    add a csrf token function to jinja template

--- epen/jinja.py
+++ epen/jinja.py
@@ -1,11 +1,21 @@
 import datetime
 import babel.dates
+from flask import session
 from .app import app
 
 def format_datetime(value, format="yyyy-MM-dd"):
     return babel.dates.format_datetime(value, format)
 
 app.jinja_env.filters["datetime"] = format_datetime
+
+def generate_csrf_token(input=True):
+    if "_csrf_token" not in session:
+        session["_csrf_token"] = some_random_string()
+    if input:
+        return "<input name=_csrf_token type=hidden value=\"%s\">" % (session['_csrf_token'],)
+    return session[""]
+
+app.jinja_env.globals["csrf_input"] = generate_csrf_token
 
 def register_blog():
     blog = {}
