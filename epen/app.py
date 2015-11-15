4d924289aefadcb1e37ea3ed8d0bc176b8667158
Guangyu Zhang <zguangyu123@gmail.com>
2015-11-15 17:58:08 +0800

    add a core template folder for admin pages

--- epen/app.py
+++ epen/app.py
@@ -1,9 +1,15 @@
 from flask import Flask
 from flask.ext.pymongo import PyMongo
-from jinja2 import FileSystemLoader
+from flask.ext.login import LoginManager
+import jinja2
 
 app = Flask("epen")
 app.config.from_pyfile("config.py")
-app.jinja_loader = FileSystemLoader("themes/%s" % (app.config["THEME"],))
+_my_loader = jinja2.ChoiceLoader([
+    app.jinja_loader,
+    jinja2.FileSystemLoader("themes/%s" % (app.config["THEME"],)),
+    jinja2.FileSystemLoader("core/"),
+])
+app.jinja_loader = _my_loader
 
 mongo = PyMongo(app)
